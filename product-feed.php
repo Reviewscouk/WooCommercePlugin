11ddcfb03d1fd4111dba5836ddd3c9c79cb53823
Liam <liam@simplywebsites.com>
2017-01-05 16:17:21 +0000

    Add Additional Columns to Product Feed

--- product-feed.php
+++ product-feed.php
@@ -5,7 +5,7 @@ $args = array('post_type' =>'product', 'showposts'=>1000);
 
 $products = get_posts($args);
 
-$productArray[] = ['sku', 'name', 'image_url', 'link', 'mpn'];
+$productArray[] = ['sku', 'name', 'image_url', 'link', 'mpn', 'woocommerce_product_sku','woocommerce_product_id'];
 
 foreach ($products as $product)
 {
@@ -13,7 +13,9 @@ foreach ($products as $product)
 	$_pf      = new WC_Product_Factory();
 	$_product = $_pf->get_product($product->ID);
 
-	$sku        = get_option('product_identifier') == 'id'? $product->ID : $_product->get_sku();
+	$woocommerce_sku = $_product->get_sku();
+	$woocommerce_id = $_product->ID;
+	$sku    = get_option('product_identifier') == 'id'? $woocommerce_id : $woocommerce_sku;
 	$image_link = '';
 
 	$image_id = $_product->get_image_id();
@@ -25,7 +27,7 @@ foreach ($products as $product)
 
 	if ($_product->product_type == 'simple')
 	{
-		$productArray[] = [$sku, $product->post_title, $image_url, get_permalink($product->ID), $sku];
+		$productArray[] = [$sku, $product->post_title, $image_url, get_permalink($product->ID), $sku, $woocommerce_sku, $woocommerce_id];
 	}
 	else if ($_product->product_type == 'variable')
 	{
@@ -33,7 +35,7 @@ foreach ($products as $product)
 
 		foreach ($available_variations as $variation)
 		{
-			$productArray[] = [ (get_option('product_identifier') == 'id'? $variation['variation_id'] : $variation['sku']), $product->post_title, $image_url, get_permalink($product->ID), $variation['sku']];
+			$productArray[] = [ (get_option('product_identifier') == 'id'? $variation['variation_id'] : $variation['sku']), $product->post_title, $image_url, get_permalink($product->ID), $variation['sku'], $variation['sku'], $variation['variation_id']];
 		}
 	}
 }
