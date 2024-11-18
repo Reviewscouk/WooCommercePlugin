416f6311711d34f13523e504361cc06ed20c6758
Adam Lammie <adam.l@reviews.io>
2024-11-18 15:17:48 +0000

    Fix javascript formatting of write review button in Polaris.

--- woocommerce-reviews.php
+++ woocommerce-reviews.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
  * Author: Reviews.co.uk
  * License: GPLv3 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
- * Version: 1.3.9
+ * Version: 1.3.10
  *
  * WC requires at least: 3.0.0
  * WC tested up to: 8.0.3
@@ -41,7 +41,7 @@ add_action('before_woocommerce_init', 'declare_wc_compatibility');
  */
 function reviewsio_admin_scripts()
 {
-    $appVersion = '1.3.9';
+    $appVersion = '1.3.10';
     // Register scripts
     wp_enqueue_script('reviewsio-admin-script', plugins_url('/js/admin-script.js', __FILE__), [], $appVersion, false);
     wp_enqueue_script('reviewsio-widget-options-script', plugins_url('/js/widget-options-script.js', __FILE__), [], $appVersion, false);
@@ -84,7 +84,7 @@ if (!class_exists('WooCommerce_Reviews')) {
 
         protected $numWidgets = 0;
         protected $richsnippet_shortcode_url = '';
-        protected $appVersion = '1.3.9';
+        protected $appVersion = '1.3.10';
 
 
         public function __construct()
@@ -1555,7 +1555,7 @@ if (!class_exists('WooCommerce_Reviews')) {
                     enable_attributes: true,
                     enable_image_gallery: true,
                     enable_percent_recommended: false,
-                    enable_write_review: "$write_a_review",
+                    enable_write_review: $write_a_review,
                     enable_ask_question: true,
                     enable_sub_header: true,
                 },
